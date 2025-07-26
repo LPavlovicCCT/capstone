b4117a6815240ceadc434513b9a84df9d7870cd2
Luka P <ptb2024001@student.cct.ie>
2025-07-26 19:42:52 +0100

    feat: added basic CRUD operations

--- api/views.py
+++ api/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializer import BookSerializer
+from .models import Book
 
-# Create your views here.
+
+class BookListView(APIView):
+    # GET a list of all books from the server
+    def get(self, request, **kwargs):
+        all_books = Book.objects.all()
+        serializer = BookSerializer(all_books, many=True)
+        return Response(serializer.data)
+    
+class BookIndividualView(APIView):
+
+    # GET a book from a list
+    def get(self, request, isbn, **kwargs):
+        book = Book.objects.get(isbn=isbn)
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
+
+    # UPDATE a book
+    def patch(self, request, isbn, **kwargs):
+        book = Book.objects.get(isbn=isbn)
+        serializer = BookSerializer(book, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    # DELETE a book
+    def delete(self, request, isbn, **kwargs):
+        book = Book.objects.get(isbn=isbn)
+        book.delete()
+        return Response()
